22a95e1500c8fffd014dcfb3db7b216f85ae3e5f
Chris <chris@midnight>
2026-03-22 18:40:02 -0700

    Initial Commit

---
+++ SignalGenerator/SignalGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignalGeneratorApp.swift
+//  SignalGenerator
+//
+//  Created by Chris on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SignalGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
